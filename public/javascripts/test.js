559292ab5665851baae8df3187f94808c6328b6d
Binh Nguyen <b.nguyen.sn@gmail.com>
2016-05-11 01:30:26 +0100

    - Updated test.js controller

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,27 +2,30 @@ var app = angular.module("testApp", []);
 
 app.controller("testCtrl", function($scope, $http) {
     
-    var count = 0;
+
     $scope.btnClick = function() {
-        count += 1;
-        $scope.statusDisplay = "Count = " + count;
+        var rand = 7;
+        $scope.statusDisplay = "Sum components for " + rand + ": " + findD2(rand).toString();
     };
 
-    // Find unique permutations of integer
-    function NNode(number, chainEnd) {
-        this.n = number;
-        this.e = chainEnd;
-        this.v = {};
 
-        this.branchOut = function(n) {
-            var nNode = this;
-            if (n > 1) {
-                var newN = n - 1;
-                nNode.v["1"] = new NNode(1, false);
-                nNode.v[newN] = new NNode(newN, true);
-                nNode.v[newN].branchOut(newN);
-            }
+
+
+    var res = {};
+    function findPI(number) {
+        for (var i = number; i > 0; i--) {
+            res[i] = [];
+
+
         }
     }
-    
+
+    function findD2(sum) {  // sum = integer only
+        var res = [];
+        for (var i = sum - 1; i >= sum / 2; i--) {
+            res.push([i, sum - i]);
+        }
+        return res;
+    }
+
 });
