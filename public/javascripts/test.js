5ba5a63d728193d510aa07509f1731dafa02f981
Binh Nguyen <b.nguyen.sn@gmail.com>
2016-05-11 13:47:01 +0100

    Updated test.js with new algorithm

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,31 +1,38 @@
 var app = angular.module("testApp", []);
 
 app.controller("testCtrl", function($scope, $http) {
-    
 
     $scope.btnClick = function() {
-        var rand = 7;
-        $scope.statusDisplay = "Sum components for " + rand + ": " + findD2(rand).toString();
+        console.log("Hello wordl");
+        findD(18);
+        $scope.statusDisplay = "Total function calls: " + count
+            + "\nTotal unique instances: " + res.length;
     };
 
-
-
-
-    var res = {};
-    function findPI(number) {
-        for (var i = number; i > 0; i--) {
-            res[i] = [];
-
-
+    var res = []; var count = 0;
+    function findD(rem, cur) {
+        count += 1;
+        var nJP;
+        if (cur == null) {
+            cur = [];
+            nJP = rem;
+        } else {
+            nJP = cur[cur.length - 1];
+        }
+        if (rem == 0) {
+            res.push(cur);
+        } else {
+            for (var i = Math.min(nJP, rem); i >= 1; i--) {
+                var newArr = cur.slice();
+                newArr.push(i);
+                // Recurse:
+                findD(rem - i,newArr);
+            }
         }
     }
 
-    function findD2(sum) {  // sum = integer only
-        var res = [];
-        for (var i = sum - 1; i >= sum / 2; i--) {
-            res.push([i, sum - i]);
-        }
-        return res;
+    function sumArr(arr) {
+        return arr.reduce(function(pV, cV) {return pV + cV});
     }
 
 });
